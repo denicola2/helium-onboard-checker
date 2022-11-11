ff96910e3e9ba0a83c6d18b8b5173e0482b39016
falconpunch <denicola2@gmail.com>
2022-11-11 12:35:22 -0500

    Create checker.sh

---
+++ checker.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# Usage: ./checker.sh 14rb2UcfS9U89QmKswpZpjRCUVCVu1haSyqyGY486EvsYtvdJmR "SyncroB.it" user@gmail.com
+
+maker_id=$1
+maker_name=$2
+mail_to=$3
+mail_name=`echo $mail_to | cut -d '@' -f1`
+sent_file=$maker_name.$mail_name.sent_mail
+cur_file=cur.$maker_name.$mail_name.json
+
+curl -s https://api.helium.io/v1/accounts/$maker_id > $cur_file
+if [ $? -eq 0 ]; then
+  dc_balance=`jq .data.dc_balance $cur_file`
+  onboards_remain=$((dc_balance / 5000000))
+  if [ $onboards_remain -gt 0 ]; then
+          if [ ! -f $sent_file ]; then
+                echo "Subject: Heads up! $maker_name has some onboards remaining!" > mail.$mail_name.txt
+                echo "$maker_name has $onboards_remain onboards remaining. Get to it!" >> mail.$mail_name.txt
+                if [ ! -z $mail_to ]; then
+                        sendmail $mail_to < mail.$mail_name.txt
+                        if [ $? -ne 0 ]; then
+                                echo "Failed to send mail to $mail_to"
+                        else
+                                touch $sent_file
+                        fi
+                fi
+          fi
+  else
+        rm -f $sent_file
+  fi
+fi
